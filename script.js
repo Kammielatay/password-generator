b176f1c95e082a3558593dcc6ba3afd5b4399eb4
Kameshia Ingram <Kameshialatay@gmail.com>
2020-11-24 15:27:10 -0700

    added prompts

--- script.js
+++ script.js
@@ -11,7 +11,24 @@ function writePassword() {
 }
 
 // make a function generatePassword below here 
+function generatePassword() {
+  // prompts and confirmations
+  let input = prompt("How long would you like your password to be? \n(please pick a value from 8 - 128)");
+  let upperCase = confirm("Would you like any uppercase letters in your password?");
+  let numbers = confirm("Would you like numbers in your password?");
+  let specialChar = confirm("Would you like special characters?")
 
+
+
+
+
+
+
+
+
+
+
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
