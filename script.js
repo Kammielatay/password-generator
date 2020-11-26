bd174019c33bc737268595c6ba26e9f2b2cdfa44
Kameshia Ingram <Kameshialatay@gmail.com>
2020-11-26 01:01:50 -0700

    added logical conditioning

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ function writePassword() {
 // make a function generatePassword below here 
 function generatePassword() {
   let customPassword = [];
+  let availableChar = [];
+  let neededChar = [];
 
   // prompts and confirmations
   let input = prompt("How long would you like your password to be?\n(please pick a value from 8 - 128)");
@@ -20,6 +22,11 @@ function generatePassword() {
   let lowercase = confirm("Would you like any lowercase letters in your password?");
   let numbers = confirm("Would you like numbers in your password?");
   let special = confirm("Would you like special characters?");
+
+    if (!uppercase && !lowercase && !numbers && !special){
+      alert('Please confirm at least one character type')
+    } 
+     
   
   
   // gathering random item from arrays
@@ -31,35 +38,35 @@ function generatePassword() {
   
   let specialArr = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '.', '/', ':', ';', '<', '>', '=', '?', '@', '[', ']', '\\', '^', '_', '`', '{', '|', '}', '~', ' ']
   
-  
 
-  for (let i = 0; i < input; i++) {
-    let randomLowLetter = abcLowArr[Math.floor(Math.random() * 26)];
-    let randomCapLetter = abcCapArr[Math.floor(Math.random() * 26)];
-    let randomNum = arrNum[Math.floor(Math.random() * 10)];
-    let randomSpecialArr = specialArr[Math.floor(Math.random() * 32)];
-    // let combination = randomLowLetter + randomCapLetter + randomNum + randomSpecialArr;
-    // let randomCombination = combination[Math.floor(Math.random() * 4)];
+    if (uppercase) {
+      neededChar = neededChar.concat(abcCapArr[Math.floor(Math.random() * abcCapArr.length)])
+      availableChar = availableChar.concat(abcCapArr)
 
-    if (uppercase && lowercase && numbers && special) {
-      let combination = randomLowLetter + randomCapLetter + randomNum + randomSpecialArr;
-      let randomCombination = combination[Math.floor(Math.random() * 4)];
-      customPassword.push(randomCombination)
     }
 
-    
+    if (lowercase) {
+      neededChar = neededChar.concat(abcLowArr[Math.floor(Math.random() * abcLowArr.length)])
+      availableChar = availableChar.concat(abcLowArr)
+    }
 
+    if (numbers) {
+      neededChar = neededChar.concat(arrNum[Math.floor(Math.random() * arrNum.length)])
+      availableChar = availableChar.concat(arrNum)
+    }
+
+    if (special) {
+      neededChar = neededChar.concat(specialArr[Math.floor(Math.random() * specialArr.length)])
+      availableChar = availableChar.concat(specialArr)
+    }
+
+    for (let i = 0; i < input; i++) {
+      let randomChar = availableChar[Math.floor(Math.random() * availableChar.length)];
+      customPassword.push(randomChar)
   
-    
+    }
 
-
-
-  }
-
-  return customPassword.join('');
-
-
-
+  return customPassword.join('')
 
 }
 
